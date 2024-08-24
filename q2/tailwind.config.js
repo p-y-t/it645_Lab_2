7db157aff1498333eb75419b76cc12996888697b
Priyamkumar Trambadia <202312083@daiict.ac.in>
2024-08-24 17:17:17 +0530

    q1 and q2

---
+++ q2/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#343a40',
+        secondary: '#007bff',
+      },
+    },
+  },
+  plugins: [],
+}
+
